c4bf5eb9a68996a3c22cbc9bc11a3a97e6dadde7
lork27 <g.lorca.lamadrid@gmail.com>
2022-11-27 11:06:07 -0400

    feat: map style match web colors

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,27 @@
+// import { AspectRatio } from "@mantine/core";
 import { AspectRatio } from "@mantine/core";
+import { Marker, Popup, MapContainer, TileLayer, useMap } from "react-leaflet";
+
+const center = [18.2, -66.5];
+const zoom = 10;
 
 export default function HomePage() {
   return (
     <AspectRatio ratio={16 / 8}>
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d314562.0940041943!2d-66.5255561876427!3d18.209551633931955!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8c0296261b92a7f9%3A0xf336ec2818049b1a!2sPuerto%20Rico!5e0!3m2!1sen!2spr!4v1669556052561!5m2!1sen!2spr"></iframe>
+      <iframe src="https://api.maptiler.com/maps/8d1659b0-75bc-4a7e-bf3f-cf5fccd4e0ef/?key=qaioH87PxUIdnaDl3ycV#9.5/18.218/-66.348"></iframe>
     </AspectRatio>
   );
+  // return (
+  //   <MapContainer
+  //     center={center}
+  //     zoom={10}
+  //     scrollWheelZoom={false}
+  //     style={{ width: "100vh", height: "100vh" }}
+  //   >
+  //     <TileLayer
+  //       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  //       url="https://api.maptiler.com/maps/8d1659b0-75bc-4a7e-bf3f-cf5fccd4e0ef/?key=qaioH87PxUIdnaDl3ycV#8.7/18.21840/-66.34802"
+  //     />
+  //   </MapContainer>
+  // );
 }
