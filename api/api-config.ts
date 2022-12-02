1682370218de83fbc0dcb3a4ca95ac446a17b78e
lork27 <g.lorca.lamadrid@gmail.com>
2022-12-02 00:00:17 -0400

    chore: add correct local and production api

--- api/api-config.ts
+++ api/api-config.ts
@@ -1,4 +1,4 @@
 export const URL =
   process.env.NODE_ENV === "production"
-    ? "https://us-central1-better-infra.cloudfunctions.net/api/"
-    : "http://localhost:5000/better-infra/us-central1/api";
+    ? "https://us-central1-betterinfra-2d3f6.cloudfunctions.net/api"
+    : "http://localhost:5001/betterinfra-2d3f6/us-central1/api";
